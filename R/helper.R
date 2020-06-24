042b88edab7efe37ce0ca099920fa0fb3dbec3f3
Michel Lang <michellang@gmail.com>
2020-06-24 16:10:58 +0200

    cleanup

--- R/helper.R
+++ R/helper.R
@@ -1,7 +1,7 @@
 transpose_name_value = function(li) {
   tab = rbindlist(map(li, function(x) {
     if (!is.null(x) && all(dim(x))) {
-      setNames(as.list(as.numeric(x$value)), x$name)
+      set_names(as.list(as.numeric(x$value)), x$name)
     } else {
       data.table(..dummy = 1)
     }
