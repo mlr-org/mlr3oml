4183e39ee9143f1526cf5d8f53e1835e09559a7f
Michel Lang <michellang@gmail.com>
2022-01-20 21:35:38 +0100

    chore: unify testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,6 @@
-library(testthat)
-library(checkmate)
-# library(mlr3oml)
-
-test_check("mlr3oml")
+if (requireNamespace("testthat", quietly = TRUE)) {
+  library("testthat")
+  library("checkmate")
+  library("mlr3oml")
+  test_check("mlr3oml")
+}
