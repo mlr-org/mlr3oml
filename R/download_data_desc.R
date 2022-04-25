1157aa828cb53f9c802f06ad2042e19ae74eecaa
Sebastian Fischer <sebf.fischer@gmail.com>
2022-04-25 16:36:34 +0200

    document

--- R/download_data_desc.R
+++ R/download_data_desc.R
@@ -3,7 +3,11 @@ download_data_desc = function(data_id) {
   desc = get_json(paste0(server, "/json/data/%i"), data_id,
     simplify_data_frame = FALSE
   )[[1L]]
+  desc = parse_data_desc(desc)
+  return(desc)
+}
 
+parse_data_desc = function(desc) {
   desc$format = tolower(desc$format)
   if (desc$format %nin% c("arff", "sparse_arff")) {
     stopf("Unsupported data format: %s", desc$format)
@@ -15,5 +19,13 @@ download_data_desc = function(data_id) {
   desc$upload_date = strptime(desc$upload_date, format = "%Y-%m-%dT%H:%M:%S", tz = "UTC")
   desc$processing_date = strptime(desc$processing_date, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
   # remove_named(desc, c("file_id", "description", "md5_checksum"))
-  desc
+
+  # OpenML uploaded the ignore_attributes comma-seperated
+  contains_comma = grepl(",", desc$ignore_attribute, fixed = TRUE)
+  ignore_no_comma = desc$ignore_attribute[!contains_comma]
+  ignore_comma = desc$ignore_attribute[contains_comma]
+  ignore_comma = map(ignore_comma, function(x) strsplit(x, ",")[[1L]])
+  ignore_comma = unlist(ignore_comma)
+  desc$ignore_attribute = c(ignore_no_comma, ignore_comma)
+  return(desc)
 }
