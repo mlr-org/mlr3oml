927faa76dbf73910b949ee931be0bae946bff3c7
Sebastian Fischer <sebf.fischer@gmail.com>
2022-06-15 16:20:29 +0200

    delete trash files

--- get_db_path.R
+++
@@ -1,9 +0,0 @@
-function(path, hash, extension) {
-  assert_string(path)
-  parent = switch(path, `:temp:` = tempdir(), `:user:` = R_user_dir("mlr 3db", "cache"), path)
-  if (!dir.exists(parent)) {
-    dir.create(parent, recursive = TRUE)
-  }
-  file.path(parent, sprintf("%s.%s", gsub("[^[:alnum:]._-]",
-    "_", hash), extension))
-}
--- tests/testthat/test_get_parquet.R
+++
@@ -1,7 +0,0 @@
-test_that("as_parquet_backend works", {
-  odata = OMLData$new(31, cache = FALSE, parquet = TRUE)
-  path = tempfile(fileext = ".pq")
-  b = as_parquet_backend(odata, path = path)
-
-
-})
