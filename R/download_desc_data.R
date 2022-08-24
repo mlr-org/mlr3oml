25efc2b484177550feabbda51ed2df1acc8f5a77
Sebastian Fischer <sebf.fischer@gmail.com>
2022-08-24 12:36:44 +0200

    chore: cleanup and simplifications

--- R/download_desc_data.R
+++ R/download_desc_data.R
@@ -14,9 +14,8 @@ parse_desc_data = function(desc) {
   desc$default_target_attribute = make.names(desc$default_target_attribute)
   desc$upload_date = strptime(desc$upload_date, format = "%Y-%m-%dT%H:%M:%S", tz = "UTC")
   desc$processing_date = strptime(desc$processing_date, format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
-  # remove_named(desc, c("file_id", "description", "md5_checksum"))
 
-  # OpenML uploaded the ignore_attributes comma-seperated
+  # OpenML (sometimes) uploaded the ignore_attributes comma-seperated
   ignore_attribute = map(desc$ignore_attribute, function(x) strsplit(x, ",")[[1L]])
   desc$ignore_attribute = unlist(ignore_attribute)
   return(desc)
--- R/download_prediction.R
+++ R/download_prediction.R
@@ -1,8 +1,7 @@
 download_prediction = function(run_id, server, desc = download_desc_run(run_id, server)) {
   is_prediction = which(grepl("prediction", desc$output_data$file$name))
   if (length(is_prediction) > 1L) {
-    warningf("More than one prediction file, using the first.")
-    is_prediction = is_prediction[[1L]]
+    stopf("More than one prediction file on OpenML.")
   }
 
   url = desc$output_data$file$url[is_prediction]
