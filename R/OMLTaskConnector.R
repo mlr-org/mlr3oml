3596810ff6bd3791b45ec38cbf9e5c31dc421e8e
Sebastian Fischer <sebf.fischer@gmail.com>
2022-09-15 15:58:02 +0200

    fix: add missing arguments to connectors

--- R/OMLTaskConnector.R
+++ R/OMLTaskConnector.R
@@ -1,12 +1,19 @@
 OMLTaskConnector = function(task_id = NULL, data_id = NULL, target_names = NULL,
-  cache = getOption("mlr3oml.cache", FALSE), parquet = getOption("mlr3oml.parquet", FALSE)) {
+  cache = getOption("mlr3oml.cache", FALSE), parquet = getOption("mlr3oml.parquet", FALSE),
+  test_server = getOption("mlr3oml.test_server", FALSE)) {
   if (!xor(is.null(task_id), is.null(data_id))) {
     stop(errorCondition("Either 'task_id' (x)or 'data_id' must be provided", class = "missingDefaultError"))
   }
 
   if (!is.null(task_id)) {
-    as_task(OMLTask$new(task_id, cache, parquet), target_names)
+    as_task(
+      OMLTask$new(task_id, cache = cache, parquet = parquet, test_server = test_server),
+      target_names
+    )
   } else {
-    as_task(OMLData$new(data_id, cache, parquet), target_names)
+    as_task(
+      OMLData$new(data_id, cache = cache,  parquet = parquet, test_server = test_server),
+      target_names
+    )
   }
 }
