be8534d0644bc7d482fbee2be0805dfc9c9caf71
Sebastian Fischer <sebf.fischer@gmail.com>
2022-09-15 16:37:54 +0200

    refactor: cleanup

--- R/utils.R
+++ R/utils.R
@@ -45,3 +45,16 @@ catf_estimation_procedure = function(estimation_procedure) {
     catf(" * Estimation: missing")
   }
 }
+
+transpose_name_value = function(li, as_integer = FALSE) {
+  converter = if (as_integer) as.integer else as.numeric
+  tab = rbindlist(map(li, function(x) {
+    if (!is.null(x) && all(dim(x))) {
+      set_names(as.list(converter(x$value)), x$name)
+    } else {
+      data.table(..dummy = 1)
+    }
+  }), fill = TRUE)
+
+  remove_named(tab, "..dummy")
+}
