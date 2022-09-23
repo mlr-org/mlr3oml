269d1c31ff373924402b52769b361a60f1485730
Sebastian Fischer <sebf.fischer@gmail.com>
2022-09-23 18:20:37 +0200

    change parquet default to TRUE

--- R/OMLTaskConnector.R
+++ R/OMLTaskConnector.R
@@ -1,5 +1,5 @@
 OMLTaskConnector = function(task_id = NULL, data_id = NULL, target_names = NULL,
-  cache = getOption("mlr3oml.cache", FALSE), parquet = getOption("mlr3oml.parquet", FALSE),
+  cache = getOption("mlr3oml.cache", FALSE), parquet = getOption("mlr3oml.parquet", TRUE),
   test_server = getOption("mlr3oml.test_server", FALSE)) {
   if (!xor(is.null(task_id), is.null(data_id))) {
     stop(errorCondition("Either 'task_id' (x)or 'data_id' must be provided", class = "missingDefaultError"))
