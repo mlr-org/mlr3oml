6a67d63ee74a92a1e438511175fbb319e83acc87
Sebastian Fischer <sebf.fischer@gmail.com>
2023-09-12 14:28:08 +0200

    cleanup [skip ci]

--- R/defaults.R
+++ R/defaults.R
@@ -1,5 +1,4 @@
 parquet_default = function() getOption("mlr3oml.parquet", FALSE)
 test_server_default = function() getOption("mlr3oml.test_server", FALSE)
 limit_default = function() getOption("mlr3oml.limit", 5000L)
-api_key_default = function(test_serve) get_api_key(get_server(test_serve))
 
