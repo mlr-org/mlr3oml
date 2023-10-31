6d92bd128d5112fd0caedfa4c3adc8d9c8f97784
Sebastian Fischer <sebf.fischer@gmail.com>
2023-10-31 15:39:10 +0100

    feat: add mlr3oml.retries option
    This can be used to make the tests more stable

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -9,5 +9,4 @@ lg = lgr::get_logger("mlr3oml")
 old_threshold = lg$threshold
 lg$set_threshold("warn")
 
-
-..old_opts = options(mlr3oml.verbose = FALSE)
+..old_opts = options(mlr3oml.verbose = FALSE, mlr3oml.retries = 10L)
