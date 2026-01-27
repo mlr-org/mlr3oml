4b2e6fbe41d9453ebf8cae192dc88dfa465d7994
Sebastian Fischer <sebf.fischer@gmail.com>
2026-01-27 09:37:45 +0100

    add uploader arg

--- tests/testthat/test_list_oml_data.R
+++ tests/testthat/test_list_oml_data.R
@@ -15,6 +15,15 @@ test_that("list_oml_data", {
   expect_data_table(list_oml_data(data_id = 999999999), nrows = 0L)
 })
 
+test_that("list_oml_data uploader filter", {
+  # uploader 1 is the OpenML admin who uploaded many datasets
+  tab = list_oml_data(uploader = 1, limit = 5)
+  expect_data_table(tab, min.rows = 1, min.cols = 10)
+
+  # non-existent uploader returns empty data.table
+  expect_data_table(list_oml_data(uploader = 999999999, limit = 5), nrows = 0L)
+})
+
 
 # listing data from test server does not work:
 # https://github.com/openml/OpenML/issues/1159#issuecomment-1225720284
